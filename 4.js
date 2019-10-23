2ec2480384d4def7645aac5d5f7a2ed0e65d8245
Todd Gladfelter <todd.gladfelter@Todds-Tiny-Mac.local>
2019-10-22 22:41:06 -0700

    fixed function name bug

--- 4.js
+++ 4.js
@@ -1,7 +1,7 @@
 // Let's get the number we're evaluating first
 let checkPandigital = (inputNumber)=>{
 	// We need to look for repeating digits to determine if pandigital
-	let inputNumberArray = num.toString().split('').sort()
+	let inputNumberArray = inputNumber.toString().split('').sort()
 	for (let i =0; i < inputNumberArray.length - 1; i++){
 		
 		if (inputNumberArray[i] === inputNumberArray[i+1]){
@@ -19,6 +19,12 @@ let checkPandigital = (inputNumber)=>{
 	console.log(inputNumberArray);
 	return true;
 }
+//checkPandigital(231)
+//checkPandigital(942)
+//checkPandigital(294883)
+
+//let number = 123456788; 
+//console.log(checkPandigital(number));
 
 
 
